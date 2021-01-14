d8e4e7a8464e4e3d35a130172b19c0586d0322e0
harold parada <haroldparada20@gmail.com>
2021-01-14 17:06:01 -0500

    added html route

---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+var path = require("path");
+
+module.exports = router;
+
+router.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname + "../../public/index.html"));
+});
+
+router.get("/exercise", (req, res) => {
+	res.sendFile(path.join(__dirname + "../../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+	res.sendFile(path.join(__dirname + "../../public/stats.html"));
+});
