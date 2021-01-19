041cc751f2d2c4fafb1bbff8601ff3a831b827d5
harold parada <haroldparada20@gmail.com>
2021-01-19 16:50:23 -0500

    added path

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,4 @@
-const Workout = require("../models/workout");
+const Workout = require("../models/workout.js");
 const router = require("express").Router();
 
 router.post("/api/workout", ({ body }, res) => {
