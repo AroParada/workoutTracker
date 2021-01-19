c3edc4a065a7983d3b61701ef208c95718395490
harold parada <haroldparada20@gmail.com>
2021-01-19 16:20:38 -0500

    changed api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
 
-router.post("/api/workout", ({ body }, res) => {
+router.post("/api/deep-thoughts", ({ body }, res) => {
 console.log(body);
 Workout.create(body, (err, data) => {
     if (err) {
@@ -12,7 +12,7 @@ Workout.create(body, (err, data) => {
 });
 });
 
-router.get("/api/workout", (req, res) => {
+router.get("/api/deep-thoughts", (req, res) => {
   Workout.find({}, 
     (err, data) => {
     if (err) {
@@ -23,7 +23,7 @@ router.get("/api/workout", (req, res) => {
   });
 });
 
-router.get("/api/workout/range", (req, res) => {
+router.get("/api/deep-thoughts/range", (req, res) => {
 	Workout.find({}, (err, data) => {
 		if (err) {
 			throw err;
@@ -35,7 +35,7 @@ router.get("/api/workout/range", (req, res) => {
 		.sort({ day: -1 });
 });
   
-router.put("/api/workout/:id", ({ params, body }, res) => {
+router.put("/api/deep-thoughts/:id", ({ params, body }, res) => {
     console.log(body)
 	db.Workout.findByIdAndUpdate(
         { _id: params.id },
