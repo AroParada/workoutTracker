94b3fb8c500adea56e5750fb50fa4c77b4e60bb0
harold parada <haroldparada20@gmail.com>
2021-01-19 15:16:39 -0500

    deploying

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts", 
+{
   useNewUrlParser: true,
-  useFindAndModify: false,
-});
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+);
 
 app.use(require("./routes/html-routes.js"));
 app.use(require("./routes/api-routes.js"));
